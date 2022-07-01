88d1b486e233a9ee65fbdaa2dfc67088da20480c
jasonc2901 <52202173+jasonc2901@users.noreply.github.com>
2022-07-01 11:03:15 +0100

    add source code for web api and error handling section

---
+++ Source Code/ShoppingListAPI/ShoppingListAPI/Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingListAPI.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Grocery> Groceries { get; set; }
+    }
+}
---
+++ Source Code/ShoppingListAPI/ShoppingListAPI/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingListAPI.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(opt => opt.UseInMemoryDatabase("ShoppingList"));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseExceptionHandler("/error-development");
+}
+else
+{
+    app.UseExceptionHandler("/error");
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
